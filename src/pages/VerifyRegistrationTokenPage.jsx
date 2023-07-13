6a7691b57a891061d6f9fa78980be4bc46ea0efc
camilascalzo <camilascalzo@hotmail.com>
2023-07-13 14:42:45 -0300

    verify account

--- src/pages/VerifyRegistrationTokenPage.jsx
+++ src/pages/VerifyRegistrationTokenPage.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MasterCard from '../components/layout/MasterCard/MasterCard';
 import { HowToRegRounded, LoginRounded } from '@mui/icons-material';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import useLoading from '../hooks/useLoading';
 import AuthService from '../services/AuthService';
 import useSnackbar from '../hooks/useSnackbar';
@@ -9,15 +9,21 @@ import ROUTES_ENUM from '../enums/routesEnum';
 
 const VerifyRegistrationTokenPage = function () {
 	const navigate = useNavigate();
+	const location = useLocation();
 	const setSnackbar = useSnackbar();
 	const setLoading = useLoading();
 	const [displayConfirmation, setDisplayConfirmation] = useState();
-	// const token = recuperar desde url
-	// http://localhost:3000/Accounts/verify-email?token=62F05474BA68DD70B50F5962F22A87FA97A3401CDBB471C7408BDD5A493BD93DE8B3806ADDB963F5C37B019658CE7D8899029A49939FCE13CF9B4BE6128E5E26
+	const [token, setToken] = useState();
+
+	useEffect(() => {
+		const params = new URLSearchParams(location.search);
+		const token = params.get('token');
+		setToken(token);
+	}, [location]);
 
 	const verifyAccount = () => {
 		setLoading(true);
-		AuthService.verifyAccount()
+		AuthService.verifyAccount(token)
 		.then(() => {
 			setLoading(false);
 			setDisplayConfirmation(true);
@@ -32,12 +38,12 @@ const VerifyRegistrationTokenPage = function () {
 	return (
 		<MasterCard
 		  logoComponent
-			headerTitle="Verificá tu cuenta"
-			headerSubtitle={!displayConfirmation ? 'Por favor, necesitamos que verifiques tu cuenta para que puedas ser parte de la comunidad de héroes' : '¡Tu cuenta ha sido verificada! Ya podés ingresar a ingresar a hungry heroes'}
+			headerTitle={!displayConfirmation ? "Verificá tu cuenta" : '¡tu cuenta fue verificada!' }
+			headerSubtitle={!displayConfirmation ? 'Por favor, necesitamos que verifiques tu cuenta para que puedas ser parte de la comunidad de héroes' : 'Ya podés ingresar usando tu email y contraseña'}
 			footerComponent
-			footerBtnTitle= {!displayConfirmation ? 'Verificar mi cuenta' : 'Ir al login'}
+			footerBtnTitle= {!displayConfirmation ? 'Verificar mi cuenta' : 'ingresar'}
 			footerBtnIcon={!displayConfirmation ? <HowToRegRounded /> :<LoginRounded /> }
-			onClick={!displayConfirmation ? () => verifyAccount() : navigate(ROUTES_ENUM.AUTH_LOGIN)}
+			onClick={!displayConfirmation ? () => verifyAccount() : () => navigate(ROUTES_ENUM.AUTH_LOGIN)}
 		>
 		</MasterCard>
 	);
